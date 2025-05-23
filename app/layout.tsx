39e8df755662ab94cb3e9c26addfe128666a0294
Ben Le Cun <benjamin.lecun@gmail.com>
2025-05-23 13:33:38 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="/logo.svg" sizes="any" />
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
